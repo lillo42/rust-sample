2c3e31bb067e447b6a96331bf33d75a46d97a8b7
Rafael Lillo <rafa_lillo@outlook.com>
2019-12-17 07:58:04 +0000

    Add read fromfile

--- quiz/src/main.rs
+++ quiz/src/main.rs
@@ -1,5 +1,9 @@
+use std::io;
+use std::io::{ Read, ErrorKind, BufRead, BufReader };
+use std::fs::File;
+use std::cmp::Ordering;
+
 struct Question {
-    id: i32,
     question: String,
     options: Vec<QuestionOption>,
     anwser: i32
@@ -37,7 +41,31 @@ struct StaticQuestionRepository { }
 
 impl QuestionRepository for FileQuestionRepository {
     fn load(&self) -> Vec<Question> {
-        panic!("NOT IMPLEMENT")
+        let file = match File::open("questions.csv") {
+            Ok(f) => f,
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => match File::create("hello.txt") {
+                    Ok(fc) => fc,
+                    Err(e) =>  panic!("Tried to create file but there was a problem: {:?}", e),
+                },
+                other_error => panic!("There was a problem opening the file {:?}", other_error)
+            }
+        };
+
+        let mut result = Vec::new();
+
+        let reader = BufReader::new(file);
+
+        for (index, line) in reader.lines().enumerate() {
+            let values = line.unwrap().split(";");
+
+
+            result.push(Question {
+
+            });
+        }
+
+        result;
     }
 }
 
@@ -47,9 +75,28 @@ impl QuestionRepository for StaticQuestionRepository {
     fn load(&self) -> Vec<Question> {
         vec![
             Question {
-                id: 1,
                 anwser: 2,
                 question: String::from("Question test"),
+                options: vec![QuestionOption {
+                    id: 1,
+                    text: String::from("test 1")
+                }, 
+                QuestionOption {
+                    id: 2,
+                    text: String::from("test 2")
+                },
+                QuestionOption {
+                    id: 3,
+                    text: String::from("test 3")
+                },
+                QuestionOption {
+                    id: 4,
+                    text: String::from("test 4")
+                }]
+            },
+            Question {
+                anwser: 3,
+                question: String::from("Question test2"),
                 options: vec![QuestionOption {
                     id: 1,
                     text: String::from("test 1")
@@ -78,11 +125,44 @@ fn main() {
     let repository = StaticQuestionRepository { };
     let questions = repository.load();
 
+    let mut right = 0;
+
     for question in questions {
         println!("{}",question.question);
 
         for option in question.options {
             println!("{} - {}",option.id, option.text);
+        };
+
+        let guess = get_number();
+
+        match guess.cmp(&question.anwser) {
+            Ordering::Equal => { 
+                println!("You are right!!!");
+                right = right + 1;
+            },
+            _ => println!("You are wrong :(")
         }
+    }
+
+    println!("You right {} questions", right);
+}
+
+
+fn get_number() -> i32 {
+    loop {
+        print!("Choose a answers: ");
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        match guess.trim().parse() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("Invalid number, try again");
+                continue
+            }
+        };
     }
 }
